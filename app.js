fb763f45c12ed46614eebd67e50ccf0ec112da0e
lucas_Mateus <102702936+Lucas-Mateus927@users.noreply.github.com>
2026-08-13 17:44:41 -0300

    Adiciona exemplo de importação do módulo

---
+++ app.js
@@ -0,0 +1,5 @@
+// Bloco 2 — Exercício 3
+// Importação e utilização da função exportada em matematica.js.
+import { somar } from "./matematica.js";
+
+console.log(`Resultado da soma: ${somar(10, 5)}`);
